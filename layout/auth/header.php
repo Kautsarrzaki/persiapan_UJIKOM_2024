b0d6ffcf13b62063e80ffb4305049ecaf05d7bb0
keyshaallya <keyshaallya07@gmail.com>
2024-03-02 18:32:26 +0700

    my changes

--- layout/auth/header.php
+++ layout/auth/header.php
@@ -4,6 +4,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>DIGITAL LIBRARY</title>
+ <link rel="icon" type="png" href="adminv/docs/assets/img/PERPUS.png">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
 <link rel="stylesheet" href="adminv/plugins/fontawesome-free/css/all.min.css">
