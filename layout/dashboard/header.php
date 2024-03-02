59a2cb19207403be034879d07b46c09de737aa0e
keyshaallya <keyshaallya07@gmail.com>
2024-03-02 17:11:30 +0700

    my changes

--- layout/dashboard/header.php
+++ layout/dashboard/header.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>DIGITAL LIBRARY</title>
-  <link rel="icon" type="image/png" href="/images/PERPUS.png">
+  <link rel="icon" type="png" href="PERPUS.png">
 
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
